1b5058e63ebaa8049946d9969a65d96bc4c2e711
yashpatil-7580 <yashpatil900596@gmail.com>
2026-04-18 20:21:59 +0530

    updated db connection to use DATABASE_URL

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,10 @@
 require('dotenv').config()
 const pg=require('pg')
 const con=new pg.Pool({
-    user:process.env.DB_USER,
-    host:process.env.DB_HOST,
-    database:process.env.DB_NAME,
-    password:process.env.DB_PASSWORD,
-    port:process.env.DB_PORT
+     connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
+
+   
 })
 
 module.exports=con;
